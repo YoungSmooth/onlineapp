1e52ab6778eea67754d75fd4fa64e1612700cb6e
unknown <chukwuonyekachi@gmail.com>
2022-06-01 22:32:16 +0100

    step-8

---
+++ lib/services/auth/auth_provider.dart
@@ -0,0 +1,15 @@
+import 'package:onlineapp/services/auth/auth_user.dart';
+
+abstract class AuthProvider {
+  AuthUser? get currentUser;
+  Future<AuthUser> logIn({
+    required String email,
+    required String password,
+  });
+  Future<AuthUser> creatUser({
+    required String email,
+    required String password,
+  });
+  Future<void> logOut();
+  Future<void> sendEmailVerification();
+}
