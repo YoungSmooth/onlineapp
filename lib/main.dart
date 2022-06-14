3d142c1e98445b496b1aff36b3f86e2607002587
unknown <chukwuonyekachi@gmail.com>
2022-06-14 05:14:33 +0100

    step-23

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,15 @@
-import 'dart:developer';
-
-import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:onlineapp/constants/routes.dart';
-import 'package:onlineapp/services/auth/auth_service.dart';
+import 'package:onlineapp/services/auth/bloc/auth_bloc.dart';
+import 'package:onlineapp/services/auth/bloc/auth_event.dart';
+import 'package:onlineapp/services/auth/bloc/auth_state.dart';
+import 'package:onlineapp/services/auth/firebase_auth_provider.dart';
 import 'package:onlineapp/views/login_views.dart';
 import 'package:onlineapp/views/notes/create_update_note_view.dart';
 import 'package:onlineapp/views/notes/notes_view.dart';
 import 'package:onlineapp/views/register_view.dart';
 import 'package:onlineapp/views/verify_email_view.dart';
-import 'package:path/path.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,9 +17,12 @@ void main() {
     MaterialApp(
       title: 'Onyion Loading',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.lightBlue,
       ),
-      home: const HomePage(),
+      home: BlocProvider<AuthBloc>(
+        create: (context) => AuthBloc(FirebaseAuthProvider()),
+        child: const HomePage(),
+      ),
       routes: {
         loginRoute: (context) => const LoginView(),
         registerRoute: (context) => const RegisterView(),
@@ -32,169 +34,165 @@ void main() {
   );
 }
 
-// class HomePage extends StatelessWidget {
-//   const HomePage({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return FutureBuilder(
-//       future: AuthService.firebase().initialize(),
-//       builder: (context, snapshot) {
-//         switch (snapshot.connectionState) {
-//           case ConnectionState.done:
-//             final user = AuthService.firebase().currentUser;
-//             if (user != null) {
-//               if (user.isEmailVerified) {
-//                 return const NotesView();
-//               } else {
-//                 return const VerifyEmailView();
-//               }
-//             } else {
-//               return const LoginView();
-//             }
-//           default:
-//             return const CircularProgressIndicator();
-//         }
-//       },
-//     );
-//   }
-// }
-
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  late final TextEditingController _controller;
-
-  @override
-  void initState() {
-    _controller = TextEditingController();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => CounterBloc(),
-      child: Scaffold(
-          appBar: AppBar(
-            title: const Text('Testing Bloc'),
-          ),
-          body: BlocConsumer<CounterBloc, CounterState>(
-            listener: (context, state) {
-              _controller.clear();
-            },
-            builder: (context, state) {
-              final invalidValue = (state is CounterStateInvalidNumber)
-                  ? state.invalidValue
-                  : '';
-              return Column(
-                children: [
-                  Text('Current value => ${state.value}'),
-                  Visibility(
-                    child: Text('invalid input: $invalidValue'),
-                    visible: state is CounterStateInvalidNumber,
-                  ),
-                  TextField(
-                    controller: _controller,
-                    decoration:
-                        const InputDecoration(hintText: 'Enter a number here'),
-                    keyboardType: TextInputType.number,
-                  ),
-                  Row(
-                    children: [
-                      TextButton(
-                        onPressed: () {
-                          context
-                              .read<CounterBloc>()
-                              .add(DecrementEvent(_controller.text));
-                        },
-                        child: const Text('-'),
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          context
-                              .read<CounterBloc>()
-                              .add(IncrementEvent(_controller.text));
-                        },
-                        child: const Text('+'),
-                      ),
-                    ],
-                  )
-                ],
-              );
-            },
-          )),
+    context.read<AuthBloc>().add(const AuthEventInitialize());
+    return BlocBuilder<AuthBloc, AuthState>(
+      builder: (context, state) {
+        if (state is AuthStateLoggedIn) {
+          return const NotesView();
+        } else if (state is AuthStateNeedsVerification) {
+          return const VerifyEmailView();
+        } else if (state is AuthStateLoggedOut) {
+          return const LoginView();
+        } else {
+          return const Scaffold(
+            body: CircularProgressIndicator(),
+          );
+        }
+      },
     );
   }
 }
 
-@immutable
-abstract class CounterState {
-  final int value;
+// class HomePage extends StatefulWidget {
+//   const HomePage({Key? key}) : super(key: key);
 
-  const CounterState(this.value);
-}
+//   @override
+//   State<HomePage> createState() => _HomePageState();
+// }
 
-class CounterStateValid extends CounterState {
-  const CounterStateValid(int value) : super(value);
-}
+// class _HomePageState extends State<HomePage> {
+//   late final TextEditingController _controller;
 
-class CounterStateInvalidNumber extends CounterState {
-  final String invalidValue;
-  const CounterStateInvalidNumber({
-    required this.invalidValue,
-    required int previousValue,
-  }) : super(previousValue);
-}
+//   @override
+//   void initState() {
+//     _controller = TextEditingController();
+//     super.initState();
+//   }
 
-@immutable
-abstract class CounterEvent {
-  final String value;
-  const CounterEvent(this.value);
-}
+//   @override
+//   void dispose() {
+//     _controller.dispose();
+//     super.dispose();
+//   }
 
-class IncrementEvent extends CounterEvent {
-  const IncrementEvent(String value) : super(value);
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return BlocProvider(
+//       create: (context) => CounterBloc(),
+//       child: Scaffold(
+//           appBar: AppBar(
+//             title: const Text('Testing Bloc'),
+//           ),
+//           body: BlocConsumer<CounterBloc, CounterState>(
+//             listener: (context, state) {
+//               _controller.clear();
+//             },
+//             builder: (context, state) {
+//               final invalidValue = (state is CounterStateInvalidNumber)
+//                   ? state.invalidValue
+//                   : '';
+//               return Column(
+//                 children: [
+//                   Text('Current value => ${state.value}'),
+//                   Visibility(
+//                     child: Text('invalid input: $invalidValue'),
+//                     visible: state is CounterStateInvalidNumber,
+//                   ),
+//                   TextField(
+//                     controller: _controller,
+//                     decoration:
+//                         const InputDecoration(hintText: 'Enter a number here'),
+//                     keyboardType: TextInputType.number,
+//                   ),
+//                   Row(
+//                     children: [
+//                       TextButton(
+//                         onPressed: () {
+//                           context
+//                               .read<CounterBloc>()
+//                               .add(DecrementEvent(_controller.text));
+//                         },
+//                         child: const Text('-'),
+//                       ),
+//                       TextButton(
+//                         onPressed: () {
+//                           context
+//                               .read<CounterBloc>()
+//                               .add(IncrementEvent(_controller.text));
+//                         },
+//                         child: const Text('+'),
+//                       ),
+//                     ],
+//                   )
+//                 ],
+//               );
+//             },
+//           )),
+//     );
+//   }
+// }
 
-class DecrementEvent extends CounterEvent {
-  const DecrementEvent(String value) : super(value);
-}
+// @immutable
+// abstract class CounterState {
+//   final int value;
 
-class CounterBloc extends Bloc<CounterEvent, CounterState> {
-  CounterBloc() : super(const CounterStateValid(0)) {
-    on<IncrementEvent>((event, emit) {
-      final integer = int.tryParse(event.value);
-      if (integer == null) {
-        emit(CounterStateInvalidNumber(
-          invalidValue: event.value,
-          previousValue: state.value,
-        ));
-      } else {
-        emit(CounterStateValid(state.value + integer));
-      }
-    });
-    on<DecrementEvent>((event, emit) {
-      final integer = int.tryParse(event.value);
-      if (integer == null) {
-        emit(CounterStateInvalidNumber(
-          invalidValue: event.value,
-          previousValue: state.value,
-        ));
-      } else {
-        emit(CounterStateValid(state.value - integer));
-      }
-    });
-  }
-}
+//   const CounterState(this.value);
+// }
+
+// class CounterStateValid extends CounterState {
+//   const CounterStateValid(int value) : super(value);
+// }
+
+// class CounterStateInvalidNumber extends CounterState {
+//   final String invalidValue;
+//   const CounterStateInvalidNumber({
+//     required this.invalidValue,
+//     required int previousValue,
+//   }) : super(previousValue);
+// }
+
+// @immutable
+// abstract class CounterEvent {
+//   final String value;
+//   const CounterEvent(this.value);
+// }
+
+// class IncrementEvent extends CounterEvent {
+//   const IncrementEvent(String value) : super(value);
+// }
+
+// class DecrementEvent extends CounterEvent {
+//   const DecrementEvent(String value) : super(value);
+// }
+
+// class CounterBloc extends Bloc<CounterEvent, CounterState> {
+//   CounterBloc() : super(const CounterStateValid(0)) {
+//     on<IncrementEvent>((event, emit) {
+//       final integer = int.tryParse(event.value);
+//       if (integer == null) {
+//         emit(CounterStateInvalidNumber(
+//           invalidValue: event.value,
+//           previousValue: state.value,
+//         ));
+//       } else {
+//         emit(CounterStateValid(state.value + integer));
+//       }
+//     });
+//     on<DecrementEvent>((event, emit) {
+//       final integer = int.tryParse(event.value);
+//       if (integer == null) {
+//         emit(CounterStateInvalidNumber(
+//           invalidValue: event.value,
+//           previousValue: state.value,
+//         ));
+//       } else {
+//         emit(CounterStateValid(state.value - integer));
+//       }
+//     });
+//   }
+// }
--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:onlineapp/enums/menu_action.dart';
+import 'package:onlineapp/services/auth/bloc/auth_bloc.dart';
+import 'package:onlineapp/services/auth/bloc/auth_event.dart';
 import 'package:onlineapp/services/cloud/cloud_note.dart';
 import 'package:onlineapp/services/cloud/firebase_cloud_storage.dart';
 import 'package:onlineapp/utilities/dialogs/logout_dialog.dart';
@@ -7,6 +9,7 @@ import 'package:onlineapp/views/notes/notes_list_view.dart';
 import '../../constants/routes.dart';
 import '../../enums/menu_action.dart';
 import '../../services/auth/auth_service.dart';
+import 'package:flutter_bloc/flutter_bloc.dart' show ReadContext;
 
 class NotesView extends StatefulWidget {
   const NotesView({Key? key}) : super(key: key);
@@ -42,11 +45,9 @@ class _NotesViewState extends State<NotesView> {
                 case MenuAction.Logout:
                   final shouldLogout = await showLogoutDialog(context);
                   if (shouldLogout) {
-                    await AuthService.firebase().logOut();
-                    Navigator.of(context).pushNamedAndRemoveUntil(
-                      loginRoute,
-                      (_) => false,
-                    );
+                    context.read<AuthBloc>().add(
+                          const AuthEventLogOut(),
+                        );
                   }
               }
             },
